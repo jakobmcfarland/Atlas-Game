e7c3e95b6596672846f16912f52e013237eda6b7
Sugar500 <38871273+Sugar500@users.noreply.github.com>
2018-05-01 15:51:59 -0700

    Room 3 Exit

--- Atlas_TextAdventure_Game/TextAdventureVisualStudio/WorldDataFactory.c
+++ Atlas_TextAdventure_Game/TextAdventureVisualStudio/WorldDataFactory.c
@@ -121,6 +121,7 @@ Room* Room3_Build()
 
 	/* Exits
 	add one or more exits to allow navigation between rooms */
+	Room_AddRoomExit(room, "south", 2);
 
 	/*  set the Flooded boolean value, interacts with player's breath*/
 	Room_Flooded(room, false);
